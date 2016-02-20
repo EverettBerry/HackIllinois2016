82d6bef43c94e98a309bdb67526f0007a48b562c
Everett Berry <epberry@purdue.edu>
2016-02-20 02:22:15 -0600

    refactored

--- maps/map.js
+++ maps/map.js
@@ -1,16 +1,6 @@
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 var overlay;
 
 function initAutocomplete() {
-
-  USGSOverlay.prototype = new google.maps.OverlayView();
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -33.8688, lng: 151.2195},
@@ -65,11 +55,9 @@ function initAutocomplete() {
       }));
 
       markers.forEach(function(marker) {
-        console.log(marker);
         marker.addListener("click", function() {
           console.log("click");
           var ph = document.getElementById("photo");
-          console.log(ph);
           ph.style.display = "block";
           
         });
@@ -85,6 +73,10 @@ function initAutocomplete() {
     map.fitBounds(bounds);
   });
 
+  markers.forEach(function(marker) {
+    console.log(marker.location);
+  });
+
   var bounds = new google.maps.LatLngBounds(
       new google.maps.LatLng(-33.86, 151.21),
       new google.maps.LatLng(-33.84, 151.24));
@@ -96,110 +88,6 @@ function initAutocomplete() {
   srcImage += 'examples/full/images/talkeetna.png';
   // [END region_getplaces]
   overlay = new USGSOverlay(bounds, srcImage, map);
-
-
-
-/** @constructor */
-function USGSOverlay(bounds, image, map) {
-
-  // Now initialize all properties.
-  this.bounds_ = bounds;
-  this.image_ = image;
-  this.map_ = map;
-
-  // Define a property to hold the image's div. We'll
-  // actually create this div upon receipt of the onAdd()
-  // method so we'll leave it null for now.
-  this.div_ = null;
-
-  // Explicitly call setMap on this overlay
-  this.setMap(map);
 }
 
-/**
- * onAdd is called when the map's panes are ready and the overlay has been
- * added to the map.
- */
-USGSOverlay.prototype.onAdd = function() {
-
-  var div = document.createElement('div');
-  div.style.border = 'none';
-  div.style.borderWidth = '0px';
-  div.style.position = 'absolute';
-
-  // Create the img element and attach it to the div.
-  var img = document.createElement('img');
-  img.src = this.image_;
-  img.style.width = '100%';
-  img.style.height = '100%';
-  div.appendChild(img);
-
-  this.div_ = div;
-
-  // Add the element to the "overlayImage" pane.
-  var panes = this.getPanes();
-  panes.overlayImage.appendChild(this.div_);
-};
-
-USGSOverlay.prototype.draw = function() {
-
-  // We use the south-west and north-east
-  // coordinates of the overlay to peg it to the correct position and size.
-  // To do this, we need to retrieve the projection from the overlay.
-  var overlayProjection = this.getProjection();
-
-  // Retrieve the south-west and north-east coordinates of this overlay
-  // in LatLngs and convert them to pixel coordinates.
-  // We'll use these coordinates to resize the div.
-  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-  // Resize the image's div to fit the indicated dimensions.
-  var div = this.div_;
-  div.style.left = sw.x + 'px';
-  div.style.top = ne.y + 'px';
-  div.style.width = (ne.x - sw.x) + 'px';
-  div.style.height = (sw.y - ne.y) + 'px';
-};
-
-USGSOverlay.prototype.onRemove = function() {
-  this.div_.parentNode.removeChild(this.div_);
-};
-
-// Set the visibility to 'hidden' or 'visible'.
-USGSOverlay.prototype.hide = function() {
-  if (this.div_) {
-    // The visibility property must be a string enclosed in quotes.
-    this.div_.style.visibility = 'hidden';
-  }
-};
-
-USGSOverlay.prototype.show = function() {
-  if (this.div_) {
-    this.div_.style.visibility = 'visible';
-  }
-};
-
-USGSOverlay.prototype.toggle = function() {
-  if (this.div_) {
-    if (this.div_.style.visibility === 'hidden') {
-      this.show();
-    } else {
-      this.hide();
-    }
-  }
-};
-
-// Detach the map from the DOM via toggleDOM().
-// Note that if we later reattach the map, it will be visible again,
-// because the containing <div> is recreated in the overlay's onAdd() method.
-USGSOverlay.prototype.toggleDOM = function() {
-  if (this.getMap()) {
-    // Note: setMap(null) calls OverlayView.onRemove()
-    this.setMap(null);
-  } else {
-    this.setMap(this.map_);
-  }
-};
-
-}
+google.maps.event.addDomListener(window, 'load', initAutocomplete);
